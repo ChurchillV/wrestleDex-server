24a9687102c12b7b67e0f415823411a5764aa5fe
ChurchillV <vincechurchillankrah@gmail.com>
2023-12-01 22:10:44 +0000

    Search query, route and controller

--- Controllers/GetWrestlers.js
+++ Controllers/GetWrestlers.js
@@ -6,7 +6,8 @@ module.exports.GetAllWrestlers = (req, res) => {
         if(error) {
             throw error;
         }
-        res.status(200).json(results.rows);
+        res.status(200)
+           .json(results.rows);
         console.log(`Wrestlers returned successfully`);
     });
 };
@@ -20,15 +21,33 @@ module.exports.GetWrestlerById = (req, res) => {
         }
 
         if(results.rows.length == 0) {
-            res.status(404).json({message : `No wrestler with id ${id} found`});
+            res.status(404)
+               .json({message : `No wrestler with id ${id} found`});
             console.log(`Search for wrestler with id ${id} failed`);
         } else {
-            res.status(200).json(results.rows);
+            res.status(200)
+               .json(results.rows);
             console.log(`Wrestler ${id} returned successfully`);            
         }
     })
 }
 
-module.exports.GetWrestlerByName = (req, res) => {
-    
+module.exports.GetWrestlersByName = (req, res) => {
+    const name = req.query.name;
+
+    pool.query(WrestlerProfileQueries.getWrestlersByName, [`%${name}%`], (error, results) => {
+        if(error) {
+            throw error;
+        }
+
+        if(results.rows.length == 0) {
+            res.status(404)
+               .json({message : `No wrestler ${name} found`});
+            console.log(`No wrestler with ${name} found`);
+        } else {
+            res.status(200)
+               .json(results.rows);
+               console.log(`${results.rows.length} Wrestlers found.`);
+        }
+    })
 }
--- Routes/GetRoutes.js
+++ Routes/GetRoutes.js
@@ -1,10 +1,11 @@
 // Importing controllers to handle routes
-const { GetAllWrestlers, GetWrestlerById } = require('../Controllers/GetWrestlers');
+const { GetAllWrestlers, GetWrestlerById, GetWrestlersByName } = require('../Controllers/GetWrestlers');
 
 const router = require('express').Router();
 
 // Routes
 router.get('/wrestlers/', GetAllWrestlers);
+router.get('/wrestlers/search', GetWrestlersByName);
 router.get('/wrestler/:id', GetWrestlerById);
 
 module.exports = router;
